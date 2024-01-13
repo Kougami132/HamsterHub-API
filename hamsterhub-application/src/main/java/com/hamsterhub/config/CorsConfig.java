f50d1df8c9b8e667de3ed1fd05c7421be6fd6424
kougami132 <kougami@qq.com>
2024-01-13 20:48:39 +0800

    修复跨域问题

--- hamsterhub-application/src/main/java/com/hamsterhub/config/CorsConfig.java
+++ hamsterhub-application/src/main/java/com/hamsterhub/config/CorsConfig.java
@@ -9,9 +9,15 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                //是否发送Cookie
+                .allowCredentials(true)
+                //设置放行哪些原始域
                 .allowedOrigins("*")
-                .allowedHeaders("*")
+                //放行哪些请求方式
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS", "HEAD")
+                //.allowedMethods("*") //或者放行全部
+                //放行哪些原始请求头部信息
+                .allowedHeaders("*")
                 .maxAge(3600);
     }
 }
