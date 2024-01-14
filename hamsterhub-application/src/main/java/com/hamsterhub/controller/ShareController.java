4c72fee7bac9fae417d965a2cd095704aed52e10
kougami132 <kougami@qq.com>
2024-01-14 22:12:39 +0800

    下载分享文件返回直链

--- hamsterhub-application/src/main/java/com/hamsterhub/controller/ShareController.java
+++ hamsterhub-application/src/main/java/com/hamsterhub/controller/ShareController.java
@@ -126,7 +126,7 @@ public class ShareController {
     @ApiOperation("下载分享文件")
     @GetMapping(value = "/downloadShare")
     public Response downloadShare(@RequestParam("ticket") String ticket,
-                                  @RequestParam("key") String key,
+                                  @RequestParam(value = "key", required = false) String key,
                                   HttpServletResponse response) {
         // 分享码不存在
         if (!shareService.isExist(ticket))
@@ -150,39 +150,13 @@ public class ShareController {
         VFileDTO vFileDTO = vFileService.query(shareDTO.getVFileId());
         RFileDTO rFileDTO = rFileService.query(vFileDTO.getRFileId());
         DeviceDTO deviceDTO = deviceService.query(rFileDTO.getDeviceId());
-        String result = fileService.download(rFileDTO);
 
-        if (deviceDTO.getType() != 0) // 非本地硬盘，返回直链
-            return Response.success().data(result);
-        else // 设备本地硬盘，返回文件
-            try {
-                File file = new File(result);
-                // 将文件写入输入流
-                FileInputStream fileInputStream = new FileInputStream(file);
-                InputStream fis = new BufferedInputStream(fileInputStream);
-                byte[] buffer = new byte[fis.available()];
-                fis.read(buffer);
-                fis.close();
-
-                // 清空response
-                response.reset();
-                // 设置response的Header
-                response.setCharacterEncoding("UTF-8");
-                //Content-Disposition的作用：告知浏览器以何种方式显示响应返回的文件，用浏览器打开还是以附件的形式下载到本地保存
-                //attachment表示以附件方式下载   inline表示在线打开   "Content-Disposition: inline; filename=文件名.mp3"
-                // filename表示文件的默认名称，因为网络传输只支持URL编码的相关支付，因此需要将文件名URL编码后进行传输,前端收到后需要反编码才能获取到真正的名称
-                response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(vFileDTO.getName(), "UTF-8"));
-                // 告知浏览器文件的大小
-                response.addHeader("Content-Length", "" + file.length());
-                OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
-                response.setContentType("application/octet-stream");
-                outputStream.write(buffer);
-                outputStream.flush();
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-        return Response.success();
+        String url;
+        if (deviceDTO.getType().equals(0)) // 本地硬盘
+            url = String.format("/download?rFileId=%s&fileName=%s", rFileDTO.getId(), vFileDTO.getName());
+        else // 网盘
+            url = fileService.download(rFileDTO);
+        return Response.success().data(url);
     }
 
     @ApiOperation("取消分享文件(token)")
