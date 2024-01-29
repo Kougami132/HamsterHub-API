3f1ebe9850eb71113fe5884f747aa10f7933c279
kougami132 <kougami@qq.com>
2024-01-30 02:28:35 +0800

    下载接口断点续传

--- hamsterhub-application/src/main/java/com/hamsterhub/controller/FileController.java
+++ hamsterhub-application/src/main/java/com/hamsterhub/controller/FileController.java
@@ -16,12 +16,15 @@ import com.hamsterhub.util.SecurityUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URLEncoder;
@@ -257,6 +260,7 @@ public class FileController {
     @GetMapping(value = "/download")
     public void download(@RequestParam("ticket") String ticket,
                              @RequestParam("fileName") String fileName,
+                             HttpServletRequest request,
                              HttpServletResponse response) {
         FileLinkDTO fileLinkDTO = fileLinkService.query(ticket);
         // 直链已过期
@@ -271,33 +275,139 @@ public class FileController {
 
         String result = fileService.download(rFileDTO);
         // 返回文件
-        try {
-            File file = new File(result);
-            // 将文件写入输入流
-            FileInputStream fileInputStream = new FileInputStream(file);
-            InputStream fis = new BufferedInputStream(fileInputStream);
-            byte[] buffer = new byte[fis.available()];
-            fis.read(buffer);
-            fis.close();
+//        try {
+//            File file = new File(result);
+//            // 将文件写入输入流
+//            FileInputStream fileInputStream = new FileInputStream(file);
+//            InputStream fis = new BufferedInputStream(fileInputStream);
+//            byte[] buffer = new byte[fis.available()];
+//            fis.read(buffer);
+//            fis.close();
+//
+//            // 清空response
+//            response.reset();
+//            // 设置response的Header
+//            response.setCharacterEncoding("UTF-8");
+//            //Content-Disposition的作用：告知浏览器以何种方式显示响应返回的文件，用浏览器打开还是以附件的形式下载到本地保存
+//            //attachment表示以附件方式下载   inline表示在线打开   "Content-Disposition: inline; filename=文件名.mp3"
+//            // filename表示文件的默认名称，因为网络传输只支持URL编码的相关支付，因此需要将文件名URL编码后进行传输,前端收到后需要反编码才能获取到真正的名称
+//            response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+//            // 告知浏览器文件的大小
+//            response.addHeader("Content-Length", "" + file.length());
+//            OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
+//            response.setContentType("application/octet-stream");
+//            outputStream.write(buffer);
+//            outputStream.flush();
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
-            // 清空response
+        RandomAccessFile targetFile = null;
+        OutputStream outputStream = null;
+        try {
+            outputStream = response.getOutputStream();
             response.reset();
-            // 设置response的Header
-            response.setCharacterEncoding("UTF-8");
-            //Content-Disposition的作用：告知浏览器以何种方式显示响应返回的文件，用浏览器打开还是以附件的形式下载到本地保存
-            //attachment表示以附件方式下载   inline表示在线打开   "Content-Disposition: inline; filename=文件名.mp3"
-            // filename表示文件的默认名称，因为网络传输只支持URL编码的相关支付，因此需要将文件名URL编码后进行传输,前端收到后需要反编码才能获取到真正的名称
-            response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
-            // 告知浏览器文件的大小
-            response.addHeader("Content-Length", "" + file.length());
-            OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
-            response.setContentType("application/octet-stream");
-            outputStream.write(buffer);
+            //获取请求头中Range的值
+            String rangeString = request.getHeader(HttpHeaders.RANGE);
+
+            //打开文件
+            File file = new File(result);
+            if (file.exists()) {
+                //使用RandomAccessFile读取文件
+                targetFile = new RandomAccessFile(file, "r");
+                long fileLength = targetFile.length();
+                long requestSize = (int) fileLength;
+                //分段下载视频
+                if (StringUtils.hasText(rangeString)) {
+                    //从Range中提取需要获取数据的开始和结束位置
+                    long requestStart = 0, requestEnd = 0;
+                    String[] ranges = rangeString.split("=");
+                    if (ranges.length > 1) {
+                        String[] rangeDatas = ranges[1].split("-");
+                        requestStart = Integer.parseInt(rangeDatas[0]);
+                        if (rangeDatas.length > 1)
+                            requestEnd = Integer.parseInt(rangeDatas[1]);
+                    }
+                    if (requestEnd != 0 && requestEnd > requestStart)
+                        requestSize = requestEnd - requestStart + 1;
+                    //根据协议设置请求头
+                    response.setHeader(HttpHeaders.ACCEPT_RANGES, "bytes");
+                    response.setHeader(HttpHeaders.CONTENT_TYPE, "video/mp4");
+                    if (!StringUtils.hasText(rangeString))
+                        response.setHeader(HttpHeaders.CONTENT_LENGTH, fileLength + "");
+                    else {
+                        long length;
+                        if (requestEnd > 0) {
+                            length = requestEnd - requestStart + 1;
+                            response.setHeader(HttpHeaders.CONTENT_LENGTH, "" + length);
+                            response.setHeader(HttpHeaders.CONTENT_RANGE, "bytes " + requestStart + "-" + requestEnd + "/" + fileLength);
+                        }
+                        else {
+                            length = fileLength - requestStart;
+                            response.setHeader(HttpHeaders.CONTENT_LENGTH, "" + length);
+                            response.setHeader(HttpHeaders.CONTENT_RANGE, "bytes " + requestStart + "-" + (fileLength - 1) + "/" + fileLength);
+                        }
+                    }
+                    //断点传输下载视频返回206
+                    response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
+                    //设置targetFile，从自定义位置开始读取数据
+                    targetFile.seek(requestStart);
+                }
+                else {
+                    //如果Range为空则下载整个视频
+                    response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+                    //设置文件长度
+                    response.setHeader(HttpHeaders.CONTENT_LENGTH, String.valueOf(fileLength));
+                }
+
+                //从磁盘读取数据流返回
+                byte[] cache = new byte[4096];
+                try {
+                    while (requestSize > 0) {
+                        int len = targetFile.read(cache);
+                        if (requestSize < cache.length)
+                            outputStream.write(cache, 0, (int) requestSize);
+                        else {
+                            outputStream.write(cache, 0, len);
+                            if (len < cache.length)
+                                break;
+                        }
+                        requestSize -= cache.length;
+                    }
+                }
+                catch (IOException e) {
+                    // tomcat原话。写操作IO异常几乎总是由于客户端主动关闭连接导致，所以直接吃掉异常打日志
+                    //比如使用video播放视频时经常会发送Range为0- 的范围只是为了获取视频大小，之后就中断连接了
+                    System.out.println(e.getMessage());
+                }
+            }
+            else
+                System.out.println("文件路劲有误");
             outputStream.flush();
         }
         catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("文件传输错误");
         }
+        finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                }
+                catch (IOException e) {
+                    System.out.println("流释放错误");
+                }
+            }
+            if (targetFile != null){
+                try {
+                    targetFile.close();
+                }
+                catch (IOException e) {
+                    System.out.println("文件流释放错误");
+                }
+            }
+        }
+
     }
 
     @ApiOperation("删除文件(token)")
