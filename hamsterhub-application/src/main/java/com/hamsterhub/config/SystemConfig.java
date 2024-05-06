428bdd96fee0dec8d1a13930a0710ca61b48b190
YTTY <1830286372@qq.com>
2024-05-06 14:22:09 +0800

    fix bug

--- hamsterhub-application/src/main/java/com/hamsterhub/config/SystemConfig.java
+++ hamsterhub-application/src/main/java/com/hamsterhub/config/SystemConfig.java
@@ -1,20 +1,14 @@
 package com.hamsterhub.config;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hamsterhub.convert.SysConfigConvert;
 import com.hamsterhub.initialize.DatabaseInitialize;
 import com.hamsterhub.response.SysConfigResponse;
 import com.hamsterhub.service.dto.SysConfigDTO;
 import com.hamsterhub.service.service.SysConfigService;
 import lombok.Getter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
@@ -23,9 +17,8 @@ import java.util.Map;
 import java.util.UUID;
 
 @Component
-public class SystemConfig implements WebMvcConfigurer {
+public class SystemConfig {
 
-    private static final Logger log = LoggerFactory.getLogger(SystemConfig.class);
     @Autowired
     private SysConfigService sysConfigService;
 
